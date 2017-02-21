46a24f898e15d93fb2f5876e3144d5819ea45c6f
Ashley Davies <ashley@daviesl.uk>
2017-02-21 00:11:15 +0000

    Added new functionality to GameMap, allowing it to dynamically map functions across tiles easily. Additionally upgraded Coordinate classes with some new features that will be useful for pathfinding.

--- ShaRPG/Util/Coordinate/TileCoordinate.cs
+++ ShaRPG/Util/Coordinate/TileCoordinate.cs
@@ -1,18 +1,27 @@
 ﻿#region
 
+using System;
 using ShaRPG.Map;
 
 #endregion
 
 namespace ShaRPG.Util.Coordinate {
     public class TileCoordinate : Coordinate {
-        public TileCoordinate(int x, int y) : base(x, y) {}
+        public TileCoordinate(int x, int y) : base(x, y) { }
 
         public static implicit operator GameCoordinate(TileCoordinate tileCoordinate) {
             return new GameCoordinate(
                 (int) ((tileCoordinate.X - tileCoordinate.Y) / 2.0 * MapTile.Width),
                 (int) ((tileCoordinate.X + tileCoordinate.Y) / 2.0 * MapTile.Height)
-                );
+            );
+        }
+
+        public static TileCoordinate operator -(TileCoordinate a, TileCoordinate b) {
+            return new TileCoordinate(a.X - b.X, a.Y - b.Y);
+        }
+
+        public int ManhattanDistance(TileCoordinate other) {
+            return Math.Abs(X - other.X) + Math.Abs(Y - other.Y);
         }
     }
 }
