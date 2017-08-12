cd94b5597e07e46006d7a1a7b490f8ad1f139202
Ashley Davies <ashley@daviesl.uk>
2017-08-12 05:02:36 +0100

    Added MouseClickMessage for handling clicks on entities; added (empty) handler to the dialog component; added GameState reference in EntityManager so that it can handle clicks, added logic for forwarding clicks to entities, extracted RenderPosition for entities; added MouseOver test to entities; added prompt to DialogNode; added Overlaps test to GameCoordinate.

--- ShaRPG/Entity/EntityManager.cs
+++ ShaRPG/Entity/EntityManager.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using ShaRPG.Entity.Components;
+using ShaRPG.GameState;
 using ShaRPG.Util;
 using ShaRPG.Util.Coordinate;
 
 namespace ShaRPG.Entity {
     internal class EntityManager : IEntityIdAssigner, IClickObserver {
         public GameEntity Player => _playerId >= 0
-                                    ? _entities[_playerId]
-                                    : throw new EntityException("Unable to determine player - ID not set");
+                                        ? _entities[_playerId]
+                                        : throw new EntityException("Unable to determine player - ID not set");
 
+        private readonly StateGame _gameState;
         private readonly Dictionary<int, GameEntity> _entities;
         private int _nextId;
         private int _playerId;
 
-        public EntityManager() {
+        public EntityManager(StateGame gameState) {
+            _gameState = gameState;
             _nextId = 0;
             _playerId = -1;
             _entities = new Dictionary<int, GameEntity>();
@@ -22,8 +27,10 @@ namespace ShaRPG.Entity {
         public int GetNextId(GameEntity e) {
             if (string.Equals(e.Name, Config.PlayerName, StringComparison.CurrentCultureIgnoreCase)) {
                 if (_playerId != -1) throw new EntityException("Multiple players spawned?");
+
                 _playerId = _nextId;
             }
+
             _entities[_nextId] = e;
             return _nextId++;
         }
@@ -45,11 +52,23 @@ namespace ShaRPG.Entity {
         }
 
         public bool IsMouseOver(ScreenCoordinate coordinates) {
-            return false;
+            return _entities.Values.Aggregate(
+                false,
+                (over, e) => over | e.MouseOver(_gameState.TranslateCoordinates(coordinates))
+            );
         }
 
         public void Clicked(ScreenCoordinate coordinates) {
-            throw new NotImplementedException();
+            GameCoordinate translateCoordinates = _gameState.TranslateCoordinates(coordinates);
+            
+            foreach (GameEntity e in _entities.Values) {
+                if (e.MouseOver(translateCoordinates)) {
+                    e.SendMessage(new MouseClickMessage(translateCoordinates));
+                    
+                    // Only one entity can be clicked - return once we found it
+                    return;
+                }
+            }
         }
     }
 }
