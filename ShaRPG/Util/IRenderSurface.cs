0ecad43ceab5ae77b72768f7a11251ec10b94862
Ashley Davies <ashley@daviesl.uk>
2017-08-13 01:37:06 +0100

    Added GuiWindow with border and background, a render offset mechanism, an absolute position rendering mechanism,

--- ShaRPG/Util/IRenderSurface.cs
+++ ShaRPG/Util/IRenderSurface.cs
@@ -5,6 +5,9 @@ namespace ShaRPG.Util {
     public interface IRenderSurface {
         Vector2I Size { get; set; }
         void Render(IDrawable sprite, GameCoordinate position);
+        void Render(IDrawable sprite, ScreenCoordinate position);
+        void AddRenderOffset(Vector2I offset);
+        void SubtractRenderOffset(Vector2I offset);
         void SetCamera(ICamera gameCamera);
     }
 }
