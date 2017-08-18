0d9f63d5bf77e00fe830f24e65f516d287a104ec
Ashley Davies <ashley@daviesl.uk>
2017-08-18 03:22:02 +0100

    Inventory max size, full property, and pickup method

--- ShaRPG/Items/Inventory.cs
+++ ShaRPG/Items/Inventory.cs
@@ -2,6 +2,14 @@
 
 namespace ShaRPG.Items {
     public class Inventory {
-        private List<ItemStack> _items;
+        private const int MaxSize = 36;
+        public bool Full => _items.Count == MaxSize;
+
+        private readonly List<ItemStack> _items = new List<ItemStack>(MaxSize);
+
+        public void PickupItem(IItem item, int count = 1) {
+            if (Full) throw new InventoryException("Inventory full, item cannot be added");
+            _items.Add(new ItemStack(item, count));
+        }
     }
 }
