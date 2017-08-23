fb314dd67e61431278f41bdaa6b2168099ad1082
Ashley Davies <ashley@daviesl.uk>
2017-08-23 04:00:28 +0100

    Changed graphic to codename and added method of getting an item by codename

--- ShaRPG/Items/IItem.cs
+++ ShaRPG/Items/IItem.cs
@@ -2,7 +2,7 @@
     public interface IItem {
         int Id { get; }
         string Name { get; }
+        string Codename { get; }
         string Description { get; }
-        string Graphic { get; }
     }
 }
--- ShaRPG/Items/ItemManager.cs
+++ ShaRPG/Items/ItemManager.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Security.Permissions;
+using System.Linq;
 using System.Xml.Linq;
 
 namespace ShaRPG.Items {
@@ -9,6 +8,7 @@ namespace ShaRPG.Items {
         private readonly Dictionary<int, IItem> _itemDictionary = new Dictionary<int, IItem>();
 
         public IItem GetItem(int id) => _itemDictionary[id];
+        public IItem GetItem(string codename) => _itemDictionary.Values.FirstOrDefault(i => i.Codename == codename);
 
         public ItemManager(string path) {
             XDocument document = XDocument.Load(Path.Combine(path, "ItemData.xml"));
@@ -16,12 +16,12 @@ namespace ShaRPG.Items {
                 int.TryParse(itemElement.Attribute("Id").Value, out int id);
                 string name = itemElement.Attribute("Name").Value;
                 string type = itemElement.Attribute("Type").Value;
-                string graphic = itemElement.Attribute("Graphic").Value;
+                string codename = itemElement.Attribute("Codename").Value;
                 string description = itemElement.Element("Description").Value;
 
                 if (type == "MeleeWeapon") {
-                    _itemDictionary.Add(id, MeleeWeapon.LoadItem(id, name, description,
-                                                                 graphic, itemElement.Element("Modifiers")));
+                    _itemDictionary.Add(id, MeleeWeapon.Load(id, name, codename, description,
+                                                                 itemElement.Element("Modifiers")));
                 }
             }
         }
