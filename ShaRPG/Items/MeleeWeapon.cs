87f04c7ab095b4d5fe58bd826576e1045adfa9f5
Ashley Davies <ashley@daviesl.uk>
2017-08-23 04:25:40 +0100

    Added Sprite to items and now ItemManager expects a SpriteStore

--- ShaRPG/Items/MeleeWeapon.cs
+++ ShaRPG/Items/MeleeWeapon.cs
@@ -1,17 +1,20 @@
 ﻿using System.Xml.Linq;
 using ShaRPG.Entity;
+using ShaRPG.Service;
 
 namespace ShaRPG.Items {
     public class MeleeWeapon : AbstractItem, IWeapon {
         public int Damage { get; }
 
-        public static IItem Load(int id, string name, string codename, string description, XElement modifiers) {
+        public static IItem Load(int id, string name, string codename, string description, XElement modifiers,
+                                 ISpriteStoreService spriteStore) {
             int.TryParse(modifiers.Element("Damage")?.Value ?? "0", out int damage);
-            return new MeleeWeapon(id, name, codename, description, damage);
+            return new MeleeWeapon(id, name, codename, description, damage, spriteStore);
         }
 
-        private MeleeWeapon(int id, string name, string codename, string description, int damage)
-            : base(id, name, description, codename) {
+        private MeleeWeapon(int id, string name, string codename, string description, int damage,
+                            ISpriteStoreService spriteStore)
+            : base(id, name, description, codename, spriteStore.GetSprite($"ui_item_{codename}")) {
             Damage = damage;
         }
     }
