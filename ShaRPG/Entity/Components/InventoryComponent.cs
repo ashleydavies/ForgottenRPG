085c3df22f0e50b6ba35b256f0afa2318e590045
Ashley Davies <ashley@daviesl.uk>
2017-08-23 05:08:35 +0100

    Began work on inventory GUI, made InventoryComponent's Inventory public, added an OpenInventoryCommand (command-based system to be scrapped shortly), and all entities are now given an inventory by default.

--- ShaRPG/Entity/Components/InventoryComponent.cs
+++ ShaRPG/Entity/Components/InventoryComponent.cs
@@ -2,7 +2,7 @@
 
 namespace ShaRPG.Entity.Components {
     public class InventoryComponent : AbstractComponent {
-        private Inventory _inventory = new Inventory();
+        public Inventory Inventory = new Inventory();
 
         public InventoryComponent(GameEntity entity) : base(entity) {
             
