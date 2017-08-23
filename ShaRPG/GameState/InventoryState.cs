2caa967561b0957ad3227482034ff2fa82b9f172
Ashley Davies <ashley@daviesl.uk>
2017-08-23 23:20:27 +0100

    Inventory GUI has 30 slots rendered ready for items to populate them. Added FixedContainer GUI component. Inventory max size changed to 30.

--- ShaRPG/GameState/InventoryState.cs
+++ ShaRPG/GameState/InventoryState.cs
@@ -9,17 +9,36 @@ namespace ShaRPG.GameState {
     public class InventoryState : AbstractGameState {
         private readonly GuiWindow _guiWindow;
         private readonly Inventory _inventory;
-        private static readonly Vector2I GuiWindowSize = new Vector2I(60 * 9 + 24, 60 * 12 + 24);
-        
-        public InventoryState(Game game, Inventory inventory, Vector2I windowSize, ICamera camera, ISpriteStoreService spriteStore)
+        private static readonly Vector2I Size = new Vector2I(WindowSizeX + 24, WindowSizeY + 24);
+        private const int WindowSizeX = 60 * 9;
+        private const int WindowSizeY = 60 * 12;
+        private const int TileSize = 48;
+        private const int TilesX = 10;
+        private const int TilesY = 3;
+        private const int TilesMargin = 2;
+        private const int TilesEdgeMargin = (WindowSizeX - TilesX * (TileSize + TilesMargin)) / 2;
+
+        public InventoryState(Game game, Inventory inventory, Vector2I windowSize, ICamera camera,
+                              ISpriteStoreService spriteStore)
             : base(game, camera) {
             _inventory = inventory;
-            
-            _guiWindow = new GuiWindow(spriteStore, windowSize / 2, GuiWindowSize);
+
+            _guiWindow = new GuiWindow(spriteStore, windowSize / 2, Size);
+
+            Sprite itemSlotSprite = spriteStore.GetSprite("ui_item_slot");
+
+            for (int y = 0; y < 3; y++) {
+                for (int x = 0; x < 10; x++) {
+                    int xPosition = TilesEdgeMargin + x * (TileSize + TilesMargin);
+                    int yPosition = WindowSizeY - TilesEdgeMargin - (TilesY - y) * (TileSize + TilesMargin);
+
+                    _guiWindow.AddComponent(
+                        new FixedContainer(xPosition, yPosition, new SpriteContainer(itemSlotSprite)));
+                }
+            }
         }
-        
-        public override void Update(float delta) {
-        }
+
+        public override void Update(float delta) { }
 
         public override void Render(IRenderSurface renderSurface) {
             _guiWindow.Render(renderSurface);
@@ -29,8 +48,6 @@ namespace ShaRPG.GameState {
             if (_guiWindow.IsMouseOver(coordinates)) _guiWindow.Clicked(coordinates);
         }
 
-        public override void MouseWheelMoved(int delta) {
-            
-        }
+        public override void MouseWheelMoved(int delta) { }
     }
 }
