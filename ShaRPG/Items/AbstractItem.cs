16dc3780d78c38c76b56725396a071d59725c699
Ashley Davies <ashley@daviesl.uk>
2017-08-23 04:00:28 +0100

    Changed graphic to codename and added method of getting an item by codename

--- ShaRPG/Items/AbstractItem.cs
+++ ShaRPG/Items/AbstractItem.cs
@@ -2,14 +2,14 @@
     public class AbstractItem : IItem {
         public int Id { get; }
         public string Name { get; }
+        public string Codename { get; }
         public string Description { get; }
-        public string Graphic { get; }
 
-        public AbstractItem(int id, string name, string description, string graphic) {
+        public AbstractItem(int id, string name, string description, string codename) {
             Id = id;
             Name = name;
             Description = description;
-            Graphic = graphic;
+            Codename = codename;
         }
     }
 }
--- ShaRPG/Items/MeleeWeapon.cs
+++ ShaRPG/Items/MeleeWeapon.cs
@@ -5,14 +5,13 @@ namespace ShaRPG.Items {
     public class MeleeWeapon : AbstractItem, IWeapon {
         public int Damage { get; }
 
-        public static IItem LoadItem(int id, string name, string description, string graphic, XElement modifiers) {
+        public static IItem Load(int id, string name, string codename, string description, XElement modifiers) {
             int.TryParse(modifiers.Element("Damage")?.Value ?? "0", out int damage);
-            
-            return new MeleeWeapon(id, name, description, graphic, damage);
+            return new MeleeWeapon(id, name, codename, description, damage);
         }
 
-        private MeleeWeapon(int id, string name, string description, string graphic, int damage)
-            : base(id, name, description, graphic) {
+        private MeleeWeapon(int id, string name, string codename, string description, int damage)
+            : base(id, name, description, codename) {
             Damage = damage;
         }
     }
