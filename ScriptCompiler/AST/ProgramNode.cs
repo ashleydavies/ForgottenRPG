526fc964ef6f234f4455807b8b57f903d0692477
Ashley Davies <ashley@daviesl.uk>
2018-01-13 08:39:56 +0000

    Began work on AST formation; currently able to branch for functions and other statements.

---
+++ ScriptCompiler/AST/ProgramNode.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ScriptCompiler.AST {
+    public class ProgramNode : ASTNode {
+        private readonly List<FunctionNode> _functionNodes;
+        private readonly List<StatementNode> _statementNodes;
+        
+        public ProgramNode(List<FunctionNode> functionNodes, List<StatementNode> statementNodes) {
+            _functionNodes = functionNodes;
+            _statementNodes = statementNodes;
+        }
+    }
+}
