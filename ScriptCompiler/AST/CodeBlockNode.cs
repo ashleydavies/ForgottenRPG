a5e52dade5c59bb8c994e4cc8505795dc086c2b1
Ashley Davies-Lyons <ashley@davies.me.uk>
2018-06-05 00:43:26 +0100

    Improved parser to handle different nodes, began work on statement node parsing

---
+++ ScriptCompiler/AST/CodeBlockNode.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ScriptCompiler.AST {
+    public class CodeBlockNode {
+        private List<StatementNode> _statements;
+
+        public CodeBlockNode(List<StatementNode> statements) {
+            _statements = statements;
+        }
+    }
+}
