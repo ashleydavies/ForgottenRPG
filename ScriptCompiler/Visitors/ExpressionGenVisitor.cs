c554c27393a46b22ccd9f004f4e153e650d60a49
Ashley Davies-Lyons <ashley@daviesl.uk>
2018-09-23 00:34:10 +0100

    Implemented expression parsing, addition, multiplication, operator precedence, printint, type derivation, rudimentary register allocation, etc.

--- ScriptCompiler/Visitors/ExpressionGenVisitor.cs
+++ ScriptCompiler/Visitors/ExpressionGenVisitor.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Text;
 using ScriptCompiler.AST;
 
 namespace ScriptCompiler.Visitors {
     // TODO: Abstraction over string
+    // Returns <commands>, <result register/location>
     public class ExpressionGenVisitor : Visitor<(List<string>, string)> {
         private readonly CodeGenVisitor _codeGenVisitor;
 
@@ -17,7 +20,7 @@ namespace ScriptCompiler.Visitors {
         }
 
         public override (List<string>, string) Visit(ASTNode node) {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException(node.GetType().FullName);
         }
 
         public (List<string>, string) Visit(StringLiteralNode node) {
@@ -26,7 +29,35 @@ namespace ScriptCompiler.Visitors {
                 throw new ArgumentException();
             }
 
-            return (new List<string>(), _codeGenVisitor.StringAliases[node.String]);
+            return (new List<string>(), "!" + _codeGenVisitor.StringAliases[node.String]);
+        }
+
+        public (List<string>, string) Visit(IntegerLiteralNode node) {
+            return (new List<string>(), node.value.ToString());
+        }
+
+        public (List<string>, string) Visit(AdditionNode node) {
+            List<string> commands = new List<string>();
+            var (commandsL, resultL) = VisitDynamic(node.Left);
+            var (commandsR, resultR) = VisitDynamic(node.Right);
+            commands.AddRange(commandsL);
+            commands.AddRange(commandsR);
+            var register = _codeGenVisitor.GetRegister();
+            commands.Add($"MOV {register} {resultL}");
+            commands.Add($"ADD {register} {resultR}");
+            return (commands, register);
+        }
+        
+        public (List<string>, string) Visit(MultiplicationNode node) {
+            List<string> commands = new List<string>();
+            var (commandsL, resultL) = VisitDynamic(node.Left);
+            var (commandsR, resultR) = VisitDynamic(node.Right);
+            commands.AddRange(commandsL);
+            commands.AddRange(commandsR);
+            var register = _codeGenVisitor.GetRegister();
+            commands.Add($"MOV {register} {resultL}");
+            commands.Add($"MUL {register} {resultR}");
+            return (commands, register);
         }
     }
 }
