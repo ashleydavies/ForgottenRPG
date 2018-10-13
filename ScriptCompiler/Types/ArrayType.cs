777116ce094e02dc179d43cd93d46428b7ea9170
Ashley Davies-Lyons <ashley@daviesl.uk>
2018-10-14 00:33:17 +0100

    Flesh out type system; add proper array types and prefix types with S for clarity vs C# types

---
+++ ScriptCompiler/Types/ArrayType.cs
@@ -0,0 +1,24 @@
+﻿namespace ScriptCompiler.Types {
+    public class ArrayType : SType {
+        public readonly SType ContainedType;
+        
+        public ArrayType(SType containedType) {
+            ContainedType = containedType;
+        }
+
+        protected bool Equals(ArrayType other) {
+            return Equals(ContainedType, other.ContainedType);
+        }
+
+        public override bool Equals(object obj) {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ArrayType) obj);
+        }
+
+        public override int GetHashCode() {
+            return (ContainedType != null ? ContainedType.GetHashCode() : 0);
+        }
+    }
+}
