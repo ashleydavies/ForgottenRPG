1ac0d16546683a48b033738dc980e01b3312eb42
Ashley Davies-Lyons <ashley@daviesl.uk>
2018-10-26 21:33:50 +0100

    Implement register reusage and begin moving to model where strings are just pointers to arrays in memory instead of magic dictionary entries

---
+++ ScriptCompiler/Register.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace ScriptCompiler {
+    public class Register : IDisposable {
+        public readonly int Number;
+        private readonly Action _dispose;
+        
+        public Register(int number, Action dispose) {
+            Number = number;
+            _dispose = dispose;
+        }
+
+        public override string ToString() {
+            return $"r{Number}";
+        }
+
+        public void Dispose() {
+            _dispose();
+        }
+    }
+}
