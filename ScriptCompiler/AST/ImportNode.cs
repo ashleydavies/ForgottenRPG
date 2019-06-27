306bf4a3043105f9f4416175805170e462df455e
Ashley Davies-Lyons <ashley@davies.me.uk>
2019-06-27 03:56:58 +0100

    Add imports to scripting language; some minor tidying up

---
+++ ScriptCompiler/AST/ImportNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScriptCompiler.AST {
+    public class ImportNode : ASTNode {
+        public readonly string FileName;
+
+        public ImportNode(string fileName) {
+            FileName = fileName;
+        }
+
+        public override List<ASTNode> Children() {
+            return new List<ASTNode>();
+        }
+    }
+}
