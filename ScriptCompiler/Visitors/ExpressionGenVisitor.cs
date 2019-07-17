f1244381e305179f67d545c73efa4fdbd671d55b
Ashley Davies-Lyons <ashley@davies.me.uk>
2019-07-17 13:40:35 +0100

    Added function return values

--- ScriptCompiler/Visitors/ExpressionGenVisitor.cs
+++ ScriptCompiler/Visitors/ExpressionGenVisitor.cs
@@ -4,10 +4,12 @@ using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Text;
 using ScriptCompiler.AST;
+using ScriptCompiler.AST.Statements;
 using ScriptCompiler.AST.Statements.Expressions;
 using ScriptCompiler.AST.Statements.Expressions.Arithmetic;
 using ScriptCompiler.CompileUtil;
 using ScriptCompiler.Types;
+using static ScriptCompiler.Visitors.CodeGenVisitor;
 
 namespace ScriptCompiler.Visitors {
     // TODO: Abstraction over string
@@ -19,9 +21,21 @@ namespace ScriptCompiler.Visitors {
         // Set to false when the address of a value is desired rather than its value; signals generation to
         //   skip the final read from memory step (e.g. for struct access)
         private bool _directAccess = true;
+        // The length of the portion of the stack being used to hold function return values; this stack space is
+        //   reserved until all of the expression has finished being processed to avoid having to manage this more
+        //   granularly
+        private int _functionReturnLength = 0;
 
         public ExpressionGenVisitor(CodeGenVisitor codeGenVisitor) {
             _codeGenVisitor = codeGenVisitor;
+        }
+
+        public (List<string>, Register) GenerateCode(ASTNode node) {
+            var (commands, dest) = VisitDynamic(node);
+            
+            commands.Add(Comment($"SUB r1 {_functionReturnLength}", "End of expression"));
+            for (int i = 0; i < _functionReturnLength; i++) _codeGenVisitor.StackFrame.Popped(SType.SInteger);
+            return (commands, dest);
         }
 
         public (List<string>, Register) VisitDynamic(ASTNode node) {
@@ -54,9 +68,15 @@ namespace ScriptCompiler.Visitors {
             var returnLabelNo = _returnLabelCount++;
             commands.Add($"PUSH $return_{returnLabelNo}");
             
+            // Add space for returned data
+            var returnType = _codeGenVisitor.FTRepo.ReturnType(node.FunctionName);
+            commands.Add($"ADD r1 {returnType.Length}");
+            _codeGenVisitor.StackFrame.Pushed(returnType);
+            _functionReturnLength += returnType.Length;
+            
             // Push each of the parameters
             foreach (var arg in node.Args) {
-                var (argCommands, argReg) = new ExpressionGenVisitor(_codeGenVisitor).VisitDynamic(arg);
+                var (argCommands, argReg) = new ExpressionGenVisitor(_codeGenVisitor).GenerateCode(arg);
                 // All arguments are one word big
                 _codeGenVisitor.StackFrame.Pushed(SType.SInteger);
                 commands.AddRange(argCommands);
@@ -75,8 +95,12 @@ namespace ScriptCompiler.Visitors {
             foreach (var arg in node.Args) _codeGenVisitor.StackFrame.Popped(SType.SInteger);
             commands.Add($"SUB r1 {node.Args.Count}");
 
-            // TODO: Return values
-            return (commands, null);
+            Register returnReg = _codeGenVisitor.GetRegister();
+            commands.Add($"MOV {returnReg} r1");
+            if (_directAccess) {
+                commands.Add($"MEMREAD {returnReg} {returnReg}");
+            }
+            return (commands, returnReg);
         }
 
         public (List<string>, Register) Visit(VariableAccessNode node) {
@@ -99,6 +123,34 @@ namespace ScriptCompiler.Visitors {
                 instructions.Add($"MEMREAD {register} {register}");
             
             return (instructions, register);
+        }
+
+        public (List<string>, Register) Visit(AssignmentNode node) {
+            List<string> instructions = new List<string>();
+            
+            var leftType = new TypeDeterminationVisitor(_codeGenVisitor).VisitDynamic(node.Destination);
+            Console.WriteLine($"An assignment to a destination of size {leftType.Length}");
+
+            var (valCommands, valDest) = VisitDynamic(node.Value);
+            instructions.AddRange(valCommands);
+
+            var copyReg = _codeGenVisitor.GetRegister();
+            
+            using (valDest) {
+                // Disable direct access to get the address of the destination rather than the first word
+                bool reenable = _directAccess;
+                _directAccess = false;
+                var (destCommands, destDest) = VisitDynamic(node.Destination);
+                _directAccess = reenable;
+                instructions.AddRange(destCommands);
+                using (destDest) {
+                    // TODO: Multi-word assignments
+                    instructions.Add($"MEMWRITE {destDest} {valDest}");
+                    instructions.Add($"MOV {copyReg} {valDest}");
+                }
+            }
+
+            return (instructions, copyReg);
         }
 
         public (List<string>, Register) Visit(StructAccessNode node) {
