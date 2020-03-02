650b5e4db67d2bd65ee6d1b11cd5b8052d74c768
Ashley Davies-Lyons <ashley@davies.me.uk>
2020-03-02 01:29:55 +0000

    Add parsing for `for` statements

---
+++ ScriptCompiler/AST/Statements/ForStatementNode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using ScriptCompiler.AST.Statements.Expressions;
+
+namespace ScriptCompiler.AST.Statements {
+    public class ForStatementNode : StatementNode {
+        private readonly StatementNode? _declaration;
+        private readonly ExpressionNode? _condition;
+        private readonly ExpressionNode? _update;
+        private readonly CodeBlockNode _block;
+
+        public ForStatementNode(StatementNode? declaration, ExpressionNode? condition, ExpressionNode? update, CodeBlockNode block) {
+            _declaration = declaration;
+            _condition = condition;
+            _update = update;
+            _block = block;
+        }
+
+        public override List<ASTNode> Children() {
+            var list = new List<ASTNode> {_block};
+            if (_declaration != null) list.Add(_declaration);
+            if (_condition != null) list.Add(_condition);
+            if (_update != null) list.Add(_update);
+            return list;
+        }
+    }
+}
